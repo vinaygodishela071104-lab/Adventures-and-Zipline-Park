57ff97508df7ef65bf293194dc285156e516c279
Eshwar04 <114127387+Eshwar04@users.noreply.github.com>
2026-05-15 13:02:54 +0530

    first commit

---
+++ login.js
@@ -0,0 +1,17 @@
+
+
+/* DARK MODE */
+
+function toggleTheme(){
+
+  document.body.classList.toggle("dark-mode");
+
+}
+
+/* RTL MODE */
+
+function toggleRTL(){
+
+  document.body.classList.toggle("rtl-mode");
+
+}
